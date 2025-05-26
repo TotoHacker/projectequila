cffe8b5577c0c5d7dfdf5168476c517d41be1249
MrDorer <133075430+MrDorer@users.noreply.github.com>
2025-05-25 22:29:42 -0500

    Terminada parte de home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Info from './Sections/Informacion/Informacion'
 import Carousel from './components/Carrusel/Carrusel'
+import Home from './components/Home/Home'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <Home />
         <Info />
       <Carousel />
       </div>
