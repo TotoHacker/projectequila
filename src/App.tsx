9d5f3476443f22bc32fad55f800c6348f7aabac9
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2025-05-27 19:20:06 -0500

    Sidebar completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import Info from './Sections/Informacion/Informacion'
 import Carousel from './components/Carrusel/Carrusel'
 import Home from './components/Home/Home'
+import Sidebar from './components/Sidebar/Sidebar'
 import Header from './components/header/Header'
+import { useState } from 'react'
 
 
 function App() {
 
+  const [sidebar,setSidebar] = useState(false)
 
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <Header />
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
+        <Header setSidebar={setSidebar}/>
         <Home />
         <Info />
       <Carousel />
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,30 +1,47 @@
-{/*import { useState } from "react";*/}
+import Imagen from "../../assets/images/Logo.png";
 
-function Sidebar() {
-  {/*const [openModal, setOpenModal] = useState(false);*/}
+type Props = {
+  sidebar: boolean;
+  setSidebar: (value: boolean) => void;
+};
 
+function Sidebar({ sidebar, setSidebar }: Props) {
   return (
-        <div>
-        {/*{openModal && (*/}
-            <div className="fixed shadow-4xl shadow-black/80 rounded-2xl top-0 left-0 w-[60vw] md:w-[20vw] h-full bg-black z-50 text-white p-[2vw]">
-            <button
-                className="absolute top-4 right-4 text-2xl font-semibold">
-                X
-            </button>
-                {/*Los datos del sidebar */}
-            <nav className="mt-12 flex flex-col py-[6vh] mx-[2vw] gap-6 text-lg">
-                <a className="cursor-pointer">Inicio</a>
-                <a className="cursor-pointer">Los 177 Pueblos Mágicos</a>
-                <a className="cursor-pointer">Estados</a>
-                <a className="cursor-pointer">México Desconocido</a>
-            </nav>
-                {/*Empiezan los derechos*/} 
-            <div className="absolute bottom-4 text-xs left-[2vw] text-gray-400">
-                © 2024 Pueblos Mágicos | México Desconocido
-            </div>
-            </div>
-        {/*)}*/}
-        </div>
-    );
-    }
+    <div
+      className={`
+        fixed top-0 left-0 h-full w-[60vw]  md:w-[20vw] z-50
+        bg-black text-white p-[2vw] rounded-r-2xl  shadow-[0_0_40px_rgba(0,0,0,0.9)] 
+        transform transition-transform duration-300 ease-in-out
+        ${sidebar ? "translate-x-0" : "-translate-x-full"}
+      `}
+    >
+
+      <div className="flex mt-[2.5vh] md:mt-[1vh] items-center justify-center ">
+        <img src={Imagen} alt="Logo" className="h-[3.2vh] md:h-[5vh]" />
+      </div>
+
+      {/* Botón para cerrar */}
+      <button
+        onClick={() => setSidebar(false)}
+        className="absolute top-4 right-4 text-2xl font-semibold cursor-pointer"
+      >
+        X
+      </button>
+
+      {/* Menú */}
+      <nav className="mt-12 flex flex-col mx-[2vw] gap-6 text-lg">
+        <a className="cursor-pointer">Inicio</a>
+        <a className="cursor-pointer">Los 177 Pueblos Mágicos</a>
+        <a className="cursor-pointer">Estados</a>
+        <a className="cursor-pointer">México Desconocido</a>
+      </nav>
+
+      {/* Derechos */}
+      <div className="absolute bottom-4 text-xs left-[2vw] text-gray-400">
+        © 2024 Pueblos Mágicos | México Desconocido
+      </div>
+    </div>
+  );
+}
+
 export default Sidebar;
